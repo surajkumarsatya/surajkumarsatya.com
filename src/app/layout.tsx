1f079426c97da0a9d4917cf35166beda7bf8a7b9
Suraj <surajmail.799@gmail.com>
2026-02-10 23:48:51 +0530

    fix layout import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
-import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "@/components/Navbar";
-import { Footer } from "@/components/Footer";
+import {Navbar} from "../../src/components/Navbar";
+import {Footer} from "../../src/components/Footer";
 import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({

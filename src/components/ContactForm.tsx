6f856c11ad65daac611a4a639024764f6f3c5049
Suraj <surajmail.799@gmail.com>
2026-02-13 16:32:55 +0530

    add slash

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -28,7 +28,7 @@ export default function ContactForm() {
   e.preventDefault();
 
   try {
-    const res = await fetch("/api/contact", {
+    const res = await fetch("/api/contact/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
